03f771acd6f3e011269f41bd4d69e2fcf382cabe
Wladimirgadelha <Wladimirgadelha@ufpi.edu.br>
2024-10-25 16:33:00 -0300

    0.1

---
+++ backend/rotas/posts.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { createPost, deletePost, commentOnPost, likeUnlikePost, getAllPosts, getAllLikedPosts, getFollowingPosts, getUsersPosts } from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts)
+router.post("/create",protectRoute, createPost)
+router.delete("/delete/:id",protectRoute, deletePost)
+router.post("/like/:id",protectRoute, likeUnlikePost)
+router.post("/comment/:id",protectRoute, commentOnPost)
+router.get("/likes/:id",protectRoute, getAllLikedPosts)
+router.get("/following",protectRoute, getFollowingPosts)
+router.get("/user/:username",protectRoute, getUsersPosts)
+
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+
+import authRoutes from "./rotas/auth.js";
+import userRoutes from "./rotas/user.js";
+import postRoutes from "./rotas/posts.js";
+import notificationRoutes from "./rotas/notification.js";
+
+import connectMongoDB from "./db/connectMongo.js";
+
+dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+
+
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.json({limit:"7mb"}));
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
+app.use("/api/notifications",notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(8000, () =>{
+    console.log("Servidor ON na porta 8000");
+    connectMongoDB();
+});
