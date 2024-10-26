2c074a6070f81552cd5c4204c220127b61e8a84d
Wladimirgadelha <Wladimirgadelha@ufpi.edu.br>
2024-10-26 00:40:07 -0300

    0.1xxxx

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/notifications", notificationRoutes);
 
 // Configuração para servir o frontend em produção
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "frontend", "build")));
+	app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
