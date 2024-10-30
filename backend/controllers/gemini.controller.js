57d3376dcc8ba36a1a991b85bd414cfc115b1cf4
Wladimirgadelha <Wladimirgadelha@ufpi.edu.br>
2024-10-30 00:17:56 -0300

    0.5b

--- backend/controllers/gemini.controller.js
+++ backend/controllers/gemini.controller.js
@@ -1,4 +1,3 @@
-// backend/controllers/gemini.controller.js
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from "dotenv";
 dotenv.config();
@@ -6,28 +5,52 @@ dotenv.config();
 // Inicialize o cliente com sua chave de API
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-// Função de Chat para gerenciar interações múltiplas
+
+const INITIAL_PROMPT = "Você é Wad.IA, uma IA implementada em um projeto pessoal  de um Estudante de Ciencia da Computacao chamado Wladimir, esse projeto pessoal se trata de uma rede social, trate todos com educacao mas nao precisa de muita formalidade e tente evitar respostas longas a nao ser que seja pedido";
+
+
+let conversationHistory = [];
+
+
 export const geminiChat = async (req, res) => {
     const { question } = req.body;
+    
     try {
-        // Inicie o modelo de chat com suporte a múltiplos turnos
+        if (conversationHistory.length === 0) {
+            conversationHistory.push({ role: "user", content: INITIAL_PROMPT });
+        }
+        
+        // Adicione a nova pergunta do usuário ao histórico
+        conversationHistory.push({ role: "user", content: question });
+
+        // Inicie o modelo de chat com o histórico acumulado
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        
         const chat = model.startChat({
-            history: [
-                {
-                    role: "user",
-                    parts: [{ text: "Olá, estou aqui para ajudar!" }],
-                },
-            ],
+            history: conversationHistory.map((message) => ({
+                role: message.role,
+                parts: [{ text: message.content }]
+            })),
+            generationConfig: {
+                maxOutputTokens: 100,  // Configura o comprimento da resposta
+            },
         });
 
-        // Envie a mensagem do usuário para o chat
+        // Envie a mensagem do usuário para o chat e obtenha a resposta
         const result = await chat.sendMessage(question);
         const response = await result.response;
-        const text = response.text();
+        const botResponse = response.text();
+
+        // Adicione a resposta da IA ao histórico, com o papel "model" para representar a resposta do modelo
+        conversationHistory.push({ role: "model", content: botResponse });
+
+        // Limitar o tamanho do histórico para evitar sobrecarga (mantendo as últimas 20 mensagens)
+        if (conversationHistory.length > 20) {
+            conversationHistory = conversationHistory.slice(-20);
+        }
 
         // Retorne a resposta para o frontend
-        res.json({ answer: text });
+        res.json({ answer: botResponse });
     } catch (error) {
         console.error("Erro na API do Gemini:", error);
         res.status(500).json({ error: "Erro ao obter resposta do Gemini" });
--- frontend/src/pages/IAPage.jsx
+++ frontend/src/pages/IAPage.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import { FaArrowUp, FaArrowLeft } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { FaArrowUp } from "react-icons/fa";
 
 const IAPage = () => {
-    const navigate = useNavigate();
     const [question, setQuestion] = useState("");
     const [messages, setMessages] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -29,7 +27,7 @@ const IAPage = () => {
             setMessages((prevMessages) => [
                 ...prevMessages,
                 { role: "user", content: question },
-                { role: "bot", content: data.answer }
+                { role: "model", content: data.answer }
             ]);
 
             setQuestion("");
@@ -42,70 +40,66 @@ const IAPage = () => {
     };
 
     return (
-        <div className="flex flex-[4_4_0] border-l border-r border-gray-700 min-h-screen flex-col bg-black-900 text-white px-4">
-            {/* Barra Superior */}
-            <div className="w-full max-w-lg  fixed top-1  flex items-center p-3 bg-black-800 border-b  border-gray-700 ">
-                <button onClick={() => navigate(-1)} className="text-gray-300 hover:text-white mr-3">
-                    <FaArrowLeft className="w-5 h-5" />
-                </button>
-                <h2 className="text-lg font-semibold text-white">Conversando com Wad.IA</h2>
-                <span className="ml-auto text-gray-400 italic text-sm">Assista à mágica acontecer!</span>
-            </div>
-
-            <div className="pt-20 w-full flex flex-col items-center justify-center min-h-[calc(100vh-60px)]">
-                {!showChat ? (
-                    <div className="text-center space-y-6">
-                        <h1 className="text-4xl font-bold mb-4">Em que posso ajudar?</h1>
-                        <div className="flex items-center bg-black-800 rounded-full px-4 py-3 w-full max-w-md mb-8 shadow-md">
-                            <input
-                                type="text"
-                                value={question}
-                                onChange={(e) => setQuestion(e.target.value)}
-                                placeholder="Mensagem para Wad.IA"
-                                className="flex-1 bg-transparent text-white outline-none border-b border-gray-700 placeholder-gray-400 px-2"
-                                onKeyPress={(e) => e.key === 'Enter' && handleAskQuestion()}
-                            />
-                            <button onClick={handleAskQuestion} disabled={loading} className="text-gray-500 ml-3 hover:text-white">
-                                <FaArrowUp className="w-6 h-6" />
-                            </button>
-                        </div>
+        <div className="flex flex-[4_4_0] border-l border-r border-gray-700 items-center justify-center min-h-screen flex-col bg-black-900 text-white px-4">
+            {!showChat ? (
+                <div className="text-center space-y-6">
+                    <h1 className="text-4xl font-bold mb-4">Em que posso ajudar?</h1>
+                    <div className="flex items-center bg-black-800 rounded-full px-4 py-3 w-full max-w-md mb-8 shadow-md">
+                        <input
+                            type="text"
+                            value={question}
+                            onChange={(e) => setQuestion(e.target.value)}
+                            placeholder="Mensagem para Wad.IA"
+                            className="flex-1 bg-transparent text-white outline-none border-b border-gray-700 placeholder-gray-400 px-2"
+                            onKeyPress={(e) => e.key === 'Enter' && handleAskQuestion()}
+                        />
+                        <button onClick={handleAskQuestion} disabled={loading} className="text-gray-500 ml-3 hover:text-white">
+                            <FaArrowUp className="w-6 h-6" />
+                        </button>
                     </div>
-                ) : (
-                    <div className="w-full max-w-lg flex flex-col bg-black-800 rounded-lg shadow-lg h-full relative pt-4">
-                        {/* Área de mensagens */}
-                        <div className="flex-1 overflow-y-auto mb-4 space-y-4 p-4 pr-2">
-                            {messages.map((msg, index) => (
-                                <div
-                                    key={index}
-                                    className={`flex ${msg.role === "user" ? "justify-end" : "justify-start"}`}
-                                >
-                                    <div className={`p-3 rounded-xl max-w-xs ${
-                                        msg.role === "user" ? "bg-blue-500 text-white" : "bg-gray-700 text-white"
-                                    } shadow`}>
-                                        <p className="text-sm">{msg.content}</p>
-                                    </div>
+                </div>
+            ) : (
+                <div className="w-full max-w-lg flex flex-col bg-black-800 rounded-lg shadow-lg h-full relative">
+                    {/* Área de mensagens */}
+                    <div className="flex-1 overflow-y-auto mb-4 space-y-4 p-4 pr-2">
+                        {messages.map((msg, index) => (
+                            <div
+                                key={index}
+                                className={`flex ${msg.role === "user" ? "justify-end" : "justify-start"} items-center`} // Alinhando imagem e mensagem
+                            >
+                                {msg.role === "model" && (
+                                    <img
+                                        src="images/ia-icon.webp" // Caminho da imagem
+                                        alt="IA Icon"
+                                        className="w-10 h-10 mr-3 rounded-full" // Ajuste de estilo para a imagem
+                                    />
+                                )}
+                                <div className={`p-3 rounded-xl max-w-xs ${
+                                    msg.role === "user" ? "bg-blue-500 text-white" : "bg-gray-700 text-white"
+                                } shadow`}>
+                                    <p className="text-sm">{msg.content}</p>
                                 </div>
-                            ))}
-                            {loading && <p className="text-gray-400 text-center">Aguarde, Wad.IA está pensando...</p>}
-                        </div>
-
-                        {/* Campo de entrada fixo */}
-                        <div className="sticky bottom-0 p-3 md:p-4 border-t border-gray-700 bg-black flex">
-                            <input
-                                type="text"
-                                value={question}
-                                onChange={(e) => setQuestion(e.target.value)}
-                                placeholder="Digite sua mensagem..."
-                                className="flex-1 bg-transparent text-white outline-none placeholder-gray-400 px-2"
-                                onKeyPress={(e) => e.key === 'Enter' && handleAskQuestion()}
-                            />
-                            <button onClick={handleAskQuestion} disabled={loading} className="text-gray-500 ml-3 hover:text-white">
-                                <FaArrowUp className="w-6 h-6" />
-                            </button>
-                        </div>
+                            </div>
+                        ))}
+                        {loading && <p className="text-gray-400 text-center">Aguarde, Wad.IA está pensando...</p>}
                     </div>
-                )}
-            </div>
+
+                    {/* Campo de entrada fixo */}
+                    <div className="sticky bottom-0 p-3 md:p-4 border-t border-blue-700 bg-black flex">
+                        <input
+                            type="text"
+                            value={question}
+                            onChange={(e) => setQuestion(e.target.value)}
+                            placeholder="Digite sua mensagem..."
+                            className="flex-1 bg-transparent text-white outline-none placeholder-gray-400 px-2"
+                            onKeyPress={(e) => e.key === 'Enter' && handleAskQuestion()}
+                        />
+                        <button onClick={handleAskQuestion} disabled={loading} className="text-gray-500 ml-3 hover:text-white">
+                            <FaArrowUp className="w-6 h-6" />
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
